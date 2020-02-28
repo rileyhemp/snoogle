4739e0f26284e0e26f6e95a66a971381fdf435a9
Riley Hemphill <rileyhemp@gmail.com>
2020-02-28 14:51:34 -0500

    Getting google api working, storing data, retreiving data

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
+import axios from 'axios'
 import {
 	SafeAreaView,
 	StyleSheet,
--- src/Test.js
+++ src/Test.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-
-import {View, Text } from 'react-native';
+import axios from 'axios'
+import {View, Text, Button } from 'react-native';
+import {config} from '../config'
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default class Test extends Component {
-	componentDidMount(){
-		console.log('i am console')
+	search(){
+		
+		// this.storeData('test', 'some data')
+		this.getData('test')
+		// axios
+		// .get(`https://www.googleapis.com/customsearch/v1?key=${config.GOOGLE_API_KEY}&cx=${config.GOOGLE_CSE_ID}&q=lectures`)
+		// .then(res => console.log(res))
+		// .catch(e => console.log(e))
+	}
+	storeData = async (key, value) => {
+		console.log('hey')
+		try {
+			await AsyncStorage.setItem(key, value)
+		} catch (e) {
+			console.log(e)
+		}
+	}
+	getData = async (key) => {
+		try {
+    		const value = await AsyncStorage.getItem(key)
+    		if(value !== null) {
+				console.log(value)
+    		}
+  		} catch(e) {
+   			console.log(e)
+  		}
 	}
 	render(){
-		return <Text>I am a component</Text>
+		return <Button title="Search" onPress={()=>this.search()}/>
 	}
 }
