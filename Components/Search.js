8e6aecb9d08537ab643ce2bca3c1a9637f8970a3
Riley Hemphill <rileyhemp@gmail.com>
2020-02-28 17:34:39 -0500

    configured react devtools, parsing search results

--- Components/Search.js
+++ Components/Search.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import {View, Text, Button} from 'react-native';
 import {config} from '../config';
 import AsyncStorage from '@react-native-community/async-storage';
+import {flatten} from 'lodash';
+import {results} from './results';
 
 export default class Search extends Component {
 	constructor(props) {
@@ -10,20 +12,46 @@ export default class Search extends Component {
 		this.query = {
 			key: config.GOOGLE_API_KEY,
 			cx: config.GOOGLE_CSE_ID,
-			q: props.query,
+			q: 'best knives cooking', //props.query,
 			fields: 'items(link,pagemap(metatags(og:description)))',
 		};
 	}
-	async search() {
-		axios
-			.get(`https://www.googleapis.com/customsearch/v1`, {
-				params: {
-					...this.query,
-					start: 1,
-				},
-			})
-			.then(res => console.log(res))
-			.catch(e => console.log(e));
+	search() {
+		const searchResults = [];
+		for (let i = 0; i < 3; i++) {
+			searchResults.push(
+				new Promise((resolve, reject) => {
+					axios
+						.get(`https://www.googleapis.com/customsearch/v1`, {
+							params: {
+								...this.query,
+								prettyPrint: true,
+								start: i * 10 + 1,
+							},
+						})
+						.then(res => resolve(res.data))
+						.catch(e => reject(e));
+				}),
+			);
+		}
+		Promise.all(searchResults).then(res => {
+			this.setState({
+				results: [
+					...results[0].items,
+					...results[1].items,
+					...results[2].items,
+				],
+			});
+		});
+	}
+	test() {
+		let newResult = [
+			...results[0].items,
+			...results[1].items,
+			...results[2].items,
+		];
+
+		console.log(JSON.stringify(newResult));
 	}
 	render() {
 		return <Button title="Search" onPress={() => this.search()} />;
