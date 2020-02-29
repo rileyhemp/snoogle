4460550d92476a8b76fb356304fe42f2efba24f1
Riley Hemphill <rileyhemp@gmail.com>
2020-02-29 11:04:38 -0500

    Supressing timeout warning

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * @format
  */
 import {decode, encode} from 'base-64';
-
 if (!global.btoa) {
 	global.btoa = encode;
 }
@@ -10,8 +9,17 @@ if (!global.btoa) {
 if (!global.atob) {
 	global.atob = decode;
 }
-import {AppRegistry} from 'react-native';
+import {AppRegistry, YellowBox} from 'react-native';
+import _ from 'lodash';
 import App from './App';
 import {name as appName} from './app.json';
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = message => {
+	if (message.indexOf('Setting a timer') <= -1) {
+		_console.warn(message);
+	}
+};
 
 AppRegistry.registerComponent(appName, () => App);
