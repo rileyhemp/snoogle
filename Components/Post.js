585829a8bffb79f6a4cdf297f8e1fc228ce76061
Riley Hemphill <rileyhemp@gmail.com>
2020-02-29 16:57:55 -0500

    getting replies

--- Components/Post.js
+++ Components/Post.js
@@ -1,24 +1,33 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {getReplies} from './RedditAPI';
 import moment from 'moment';
 
 export const Post = ({post}) => {
 	const date = moment.unix(post.created).format('MMMM YYYY');
-	let previewLines = 3;
+	const [previewLines, setPreviewLines] = useState(3);
+	const onClickPost = () => {
+		previewLines ? setPreviewLines(null) : setPreviewLines(3);
+		getReplies(post.id)
+			.then(res => console.log(res))
+			.catch(err => console.log(err));
+	};
 	return (
 		<View style={styles.post}>
-			<View style={styles.header}>
-				<Text style={styles.title}>{post.title}</Text>
-			</View>
-			<View style={styles.preview}>
-				<Text numberOfLines={previewLines} style={styles.content}>
-					{post.selftext}
-				</Text>
-				<View style={styles.footer}>
-					<Text style={styles.smallText}>{post.num_comments} comments</Text>
-					<Text style={styles.smallText}>{date}</Text>
+			<TouchableOpacity onPress={() => onClickPost()}>
+				<View style={styles.header}>
+					<Text style={styles.title}>{post.title}</Text>
 				</View>
-			</View>
+				<View style={styles.preview}>
+					<Text numberOfLines={previewLines} style={styles.content}>
+						{post.selftext}
+					</Text>
+					<View style={styles.footer}>
+						<Text style={styles.smallText}>{post.num_comments} comments</Text>
+						<Text style={styles.smallText}>{date}</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
 		</View>
 	);
 };
--- Components/RedditAPI.js
+++ Components/RedditAPI.js
@@ -13,8 +13,34 @@ export const getPostsFromIDs = function(posts) {
 				//Config to get snoowrap to work with react native
 				r._nextRequestTimestamp = -1;
 				r.config({debug: true, proxies: false});
+
 				//Call the API
 				r.getContentByIds(posts)
+					.then(res => {
+						resolve(res);
+					})
+					.catch(err => reject(err));
+			})
+			.catch(err => reject(err));
+	});
+};
+export const getReplies = function(post) {
+	return new Promise((resolve, reject) => {
+		//Authenticate the app
+		snoowrap
+			.fromApplicationOnlyAuth({
+				clientId: 'S0qvSFIjP95kqw',
+				deviceId: 'DO_NOT_TRACK_THIS_DEVICE',
+				grantType: snoowrap.grantType.INSTALLED_CLIENT,
+			})
+			.then(r => {
+				//Config to get snoowrap to work with react native
+				r._nextRequestTimestamp = -1;
+				r.config({debug: true, proxies: false});
+
+				//Call the API
+				r.getSubmission(post)
+					.expandReplies()
 					.then(res => {
 						resolve(res);
 					})
