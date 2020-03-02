62d8471daa35eaf8e252dd89a54beae41c2f4c61
Riley Hemphill <rileyhemp@gmail.com>
2020-03-01 19:12:20 -0500

    Hiding comments

--- Components/Comments.js
+++ Components/Comments.js
@@ -6,26 +6,37 @@ import moment from 'moment';
 const Comment = ({comment, depth}) => {
 	const date = moment.unix(comment.created).format('MM D YY');
 	const [maxDepth, setMaxDepth] = useState(2);
+	const [hideSelf, setHideSelf] = useState(false);
 	const onPressContinue = () => {
 		setMaxDepth(10);
 	};
+	const handleHideSelf = state => {
+		setHideSelf(state);
+	};
 	return (
-		<View style={styles.comment}>
-			<View style={styles.header}>
-				<Text style={styles.smallText}>
-					{comment.score} points • {comment.author.name}
+		<View>
+			<Text style={hideSelf ? styles.hiddenLink : styles.hidden} onPress={() => handleHideSelf(false)}>
+				Hidden, tap to show...
+			</Text>
+			<View style={hideSelf ? styles.hidden : styles.comment}>
+				<View style={styles.header}>
+					<Text style={styles.smallText}>
+						{comment.score} points • {comment.author.name}
+					</Text>
+				</View>
+				<Text style={styles.body} onPress={() => handleHideSelf(true)}>
+					{comment.body}
 				</Text>
+				{comment.replies && depth < maxDepth ? (
+					comment.replies.map(comment => {
+						return <Comment key={comment.id} comment={comment} depth={depth + 1} />;
+					})
+				) : comment.replies.length > 0 ? (
+					<Text style={styles.link} onPress={onPressContinue}>
+						Continue thread...
+					</Text>
+				) : null}
 			</View>
-			<Text style={styles.body}>{comment.body}</Text>
-			{comment.replies && depth < maxDepth ? (
-				comment.replies.map(comment => {
-					return <Comment key={comment.id} comment={comment} depth={depth + 1} />;
-				})
-			) : comment.replies.length > 0 ? (
-				<Text style={styles.link} onPress={onPressContinue}>
-					Continue thread...
-				</Text>
-			) : null}
 		</View>
 	);
 };
@@ -44,6 +55,14 @@ const styles = StyleSheet.create({
 	comment: {
 		paddingLeft: 24,
 	},
+	hiddenLink: {
+		paddingLeft: 4,
+		marginLeft: 20,
+		fontSize: 12,
+		color: '#5cc8ff',
+		marginVertical: 4,
+		paddingTop: 12,
+	},
 	header: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
@@ -58,6 +77,10 @@ const styles = StyleSheet.create({
 		paddingBottom: 4,
 		color: 'white',
 		paddingRight: 8,
+	},
+	hidden: {
+		height: 0,
+		overflow: 'hidden',
 	},
 	smallText: {
 		fontSize: 12,
