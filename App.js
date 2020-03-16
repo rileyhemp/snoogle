e152dbde553f13869fb8e4afb891141c73eaf98e
Riley Hemphill <rileyhemp@gmail.com>
2020-03-15 22:12:48 -0400

    Added tab view

--- App.js
+++ App.js
@@ -1,23 +1,43 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
+import * as React from "react";
+import { SafeAreaView, StyleSheet, StatusBar, Dimensions, View } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { TabView, SceneMap } from "react-native-tab-view";
 import rootReducer from "./Redux/Reducers";
-import moment from "moment";
 import Search from "./Containers/Search";
 import Results from "./Containers/Results";
 import theme from "./theme";
+import moment from "moment";
 
 const store = createStore(rootReducer);
-
+const initialLayout = { width: Dimensions.get("window").width };
 const App: () => React$Node = () => {
+	const [index, setIndex] = React.useState(0);
+	const [routes] = React.useState([
+		{ key: "first", title: "Search" },
+		{ key: "second", title: "Favorites" }
+	]);
+	const MainPage = () => {
+		return (
+			<View style={styles.tab}>
+				<Search />
+				<Results />
+			</View>
+		);
+	};
+	const FavoritesPage = () => {
+		return <View style={styles.tab}></View>;
+	};
+	const renderScene = SceneMap({
+		first: MainPage,
+		second: FavoritesPage
+	});
 	return (
 		<>
 			<Provider store={store}>
 				<StatusBar barStyle="dark-content" />
 				<SafeAreaView style={styles.body}>
-					<Search />
-					<Results />
+					<TabView navigationState={{ index, routes }} renderScene={renderScene} onIndexChange={setIndex} initialLayout={initialLayout} />
 				</SafeAreaView>
 			</Provider>
 		</>
@@ -28,6 +48,9 @@ const styles = StyleSheet.create({
 	body: {
 		backgroundColor: theme.background,
 		height: "100%"
+	},
+	tab: {
+		flex: 1
 	}
 });
 
